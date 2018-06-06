840f48cf56d80614f89a5ac9c07da169ef66e283
lxlnihao <616231405@qq.com>
2018-06-06 17:57:18 +0800

    新增

--- tabbar/ViewController/BaseController.m
+++ tabbar/ViewController/BaseController.m
@@ -20,6 +20,7 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.edgesForExtendedLayout = UIRectEdgeNone;
+    NSLog(@"新版开发");
 }
 
 - (void)didReceiveMemoryWarning {
